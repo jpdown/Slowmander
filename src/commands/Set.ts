f106412934ea0d9548ce494892065e77d6638553
Jaden D <thepoisonedpanther@gmail.com>
2020-05-08 23:38:47 -0500

    set activity and status

--- src/commands/Set.ts
+++ src/commands/Set.ts
@@ -3,12 +3,12 @@ import { Command } from "./Command";
 import { PermissionLevel } from "./Command";
 import { PantherBot } from "../Bot";
 
-import { Message, User, GuildMember, Role } from "discord.js";
+import { Message, User, GuildMember, Role, ActivityOptions } from "discord.js";
 import { CommandUtils } from "../utils/CommandUtils";
 
 export class Set extends CommandGroup {
     constructor() {
-        super("set", PermissionLevel.Owner, "Sets various bot parameters", "", true);
+        super("set", PermissionLevel.Owner, "Sets various bot parameters", true);
 
         this.registerSubCommands();
     }
@@ -18,9 +18,12 @@ export class Set extends CommandGroup {
         this.registerSubCommand(new SetAvatar(this));
         this.registerSubCommand(new SetNickname(this));
         this.registerSubCommand(new SetOwner(this));
+        this.registerSubCommand(new SetPrefix(this));
         this.registerSubCommand(new SetVipRole(this));
         this.registerSubCommand(new SetModRole(this));
         this.registerSubCommand(new SetAdminRole(this));
+        this.registerSubCommand(new SetStatus(this));
+        this.registerSubCommand(new SetActivity(this));
     }
 }
 
@@ -125,6 +128,24 @@ class SetOwner extends Command {
     }
 }
 
+class SetPrefix extends Command {
+    constructor(group: CommandGroup) {
+        super("prefix", PermissionLevel.Owner, "Sets bot prefix", "<prefix>", true, group);
+    }
+
+    public async run(bot: PantherBot, message: Message, args: string[]): Promise<void> {
+        if(args.length < 1) {
+            await this.sendMessage("you gotta give me a prefix dumbo", message.channel);
+            return;
+        }
+
+        let prefix: string = args.join(" ");
+
+        await bot.config.setPrefix(prefix);
+        await this.sendMessage(`Prefix set to ${prefix} successfully.`, message.channel);
+    }
+}
+
 class SetVipRole extends Command {
     constructor(group: CommandGroup) {
         super("viprole", PermissionLevel.Owner, "Sets bot's VIP role", "<role>", false, group);
@@ -191,5 +212,85 @@ class SetAdminRole extends Command {
 
         await bot.config.setAdminRole(role.id);
         await this.sendMessage(`Admin role set to ${role.toString()} successfully.`, message.channel);
+    }
+}
+
+class SetStatus extends Command {
+    constructor(group: CommandGroup) {
+        super("status", PermissionLevel.Owner, "Sets bot status", "<online, away/idle, dnd, invis/offline>", true, group);
+    }
+
+    public async run(bot: PantherBot, message: Message, args: string[]): Promise<void> {
+        if(args.length < 1) {
+            await this.sendMessage("you gotta give me a status dumbo", message.channel);
+            return;
+        }
+
+        switch(args[0]) {
+            case "online":
+                await message.client.user.setStatus("online");
+                break;
+            case "away":
+            case "idle":
+                await message.client.user.setStatus("idle");
+                break;
+            case "dnd":
+                await message.client.user.setStatus("dnd");
+                break;
+            case "invis":
+            case "invisible":
+            case "offline":
+                await message.client.user.setStatus("invisible");
+                break;
+            default:
+                await this.sendMessage("you gotta give me a valid status dumbo", message.channel);
+                return;
+        }
+
+        await this.sendMessage("Status updated successfully.", message.channel);
+    }
+}
+
+class SetActivity extends Command {
+    private readonly STREAMING_URL: string = "https://twitch.tv/poisonedpanther";
+
+    constructor(group: CommandGroup) {
+        super("activity", PermissionLevel.Owner, "Sets bot activity", "<playing, streaming, listening, watching, clear> <activity string>", true, group);
+    }
+
+    public async run(bot: PantherBot, message: Message, args: string[]): Promise<void> {
+        if(args.length < 1 || (args.length < 2 && args[0] !== "clear")) {
+            await this.sendMessage("you gotta give me an acitivity dumbo", message.channel);
+            return;
+        }
+
+        let activityType: string = args.shift();
+        let activityString: string = args.join(" ");
+        let activityOptions: ActivityOptions;
+
+        switch(activityType) {
+            case "playing":
+                activityOptions = {type: "PLAYING"};
+                break;
+            case "streaming":
+                activityOptions = {type: "STREAMING", url: this.STREAMING_URL};
+                break;
+            case "listening":
+                activityOptions = {type: "LISTENING"};
+                break;
+            case "watching":
+                activityOptions = {type: "WATCHING"};
+                break;
+            case "clear":
+                activityString = "";
+                activityOptions = {};
+                break;
+            default:
+                await this.sendMessage("you gotta give me a valid activity dumbo", message.channel);
+                return;
+        }
+
+        await message.client.user.setActivity(activityString, activityOptions);
+        await this.sendMessage("Activity updated successfully.", message.channel);
     }
 }
