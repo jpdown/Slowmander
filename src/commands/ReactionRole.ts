8de3d88fc8b0f87cc900f5b5c7504f7e641f3917
Jaden Down <24631328+jpdown@users.noreply.github.com>
2020-06-07 15:06:51 -0500

    more verbose error handling for reaction roles

--- src/commands/ReactionRole.ts
+++ src/commands/ReactionRole.ts
@@ -5,7 +5,7 @@ import { PantherBot } from "../Bot";
 import { ReactionRoleObject } from "../config/ReactionRoleConfig";
 import { ReactionPaginator } from "../utils/ReactionPaginator"
 
-import { Message, Role, TextChannel, NewsChannel, Permissions, ReactionEmoji, Emoji, User, MessageReaction, Collection, ReactionCollector, GuildEmoji, Snowflake } from "discord.js";
+import { Message, Role, TextChannel, NewsChannel, Permissions, ReactionEmoji, Emoji, User, MessageReaction, Collection, ReactionCollector, GuildEmoji, Snowflake, Guild } from "discord.js";
 import { CommandUtils } from "../utils/CommandUtils";
 
 export class ReactionRoleManagement extends CommandGroup {
@@ -42,19 +42,12 @@ class AddReactionRole extends Command {
             return {sendHelp: false, command: this, message: message};
         }
 
-        //Ask for emote
-        let sentMessage: Message = await this.sendMessage("Please react on this message with the emote you would like to use.", message.channel, bot);
-        let reactions: Collection<string, MessageReaction> = await sentMessage.awaitReactions((reaction, user) => user.id === message.author.id, {time:60000, max: 1});
-
-        //Check if unicode or if we have the custom emote
-        if(reactions.size < 1) {
-            await this.sendMessage("No reaction given, cancelling.", message.channel, bot);
+        if (!await this.checkPerms(reactionRoleParsedArgs.role, reactionRoleParsedArgs.reactionMessage, message, bot)) {
             return {sendHelp: false, command: this, message: message};
         }
 
-        let emote: ReactionEmoji | GuildEmoji = reactions.first().emoji;
-        if(emote.id && emote instanceof ReactionEmoji) {
-            await this.sendMessage("I do not have access to the emote given, cancelling.", message.channel, bot);
+        let emote: ReactionEmoji | GuildEmoji = await this.getEmote(message, bot);
+        if(!emote) {
             return {sendHelp: false, command: this, message: message};
         }
 
@@ -73,12 +66,9 @@ class AddReactionRole extends Command {
             return {sendHelp: false, command: this, message: message};
         }
 
-        let success: boolean = await bot.reactionRoleManager.addReactionRole(reactionRoleObject, reactionRoleParsedArgs.reactionMessage);
+        let success: boolean = await this.addReactionRole(reactionRoleObject, reactionRoleParsedArgs.reactionMessage, message, bot);
         if(success) {
             await this.sendMessage(`Reaction role ${reactionRoleObject.name} added successfully.`, message.channel, bot);
-        }
-        else {
-            await this.sendMessage("Error adding reaction role.", message.channel, bot);
         }
 
         return {sendHelp: false, command: this, message: message};
@@ -130,6 +120,65 @@ class AddReactionRole extends Command {
 
         return({channel: channel, reactionMessage: reactionMessage, role: role, name: name});
     }
+
+    private async getEmote(message: Message, bot: PantherBot): Promise<ReactionEmoji | GuildEmoji> {
+        //Ask for emote
+        let sentMessage: Message = await this.sendMessage("Please react on this message with the emote you would like to use.", message.channel, bot);
+        let reactions: Collection<string, MessageReaction> = await sentMessage.awaitReactions((reaction, user) => user.id === message.author.id, {time:60000, max: 1});
+
+        //Check if unicode or if we have the custom emote
+        if(reactions.size < 1) {
+            await this.sendMessage("No reaction given, cancelling.", message.channel, bot);
+            return undefined;
+        }
+
+        let emote: ReactionEmoji | GuildEmoji = reactions.first().emoji;
+        if(emote.id && emote instanceof ReactionEmoji) {
+            await this.sendMessage("I do not have access to the emote given, cancelling.", message.channel, bot);
+        }
+
+        return(emote);
+    }
+
+    private async checkPerms(role: Role, reactionMessage: Message, message: Message, bot: PantherBot): Promise<boolean> {
+        //Manage roles
+        if(!role.guild.me.hasPermission(Permissions.FLAGS.MANAGE_ROLES)) {
+            await this.sendMessage("Adding reaction role failed. I do not have the Manage Roles permission.", message.channel, bot);
+            return false;
+        }
+        //If role below us
+        if(role.comparePositionTo(role.guild.me.roles.highest) > 0) {
+            await this.sendMessage("Adding reaction role failed. Invalid hierarchy, my highest role is below the given role.", message.channel, bot);
+            return false;
+        }
+        //If we can't react in the channel
+        if(!reactionMessage.guild.me.permissionsIn(reactionMessage.channel).has(Permissions.FLAGS.ADD_REACTIONS)) {
+            await this.sendMessage(`Adding reaction role failed. I do not have reaction perms in ${reactionMessage.channel.toString()}`, message.channel, bot);
+            return false;
+        }
+
+        return true;
+    }
+
+    private async addReactionRole(reactionRole: ReactionRoleObject, reactionMessage: Message, message: Message, bot: PantherBot): Promise<boolean> {
+        let dbResult: boolean = await bot.configs.reactionRoleConfig.addReactionRole(reactionRole);
+        if(!dbResult) {
+            await this.sendMessage("Adding reaction role failed.", message.channel, bot);
+            return(false);
+        }
+
+        //React to message
+        try {
+            await reactionMessage.react(reactionRole.emoteID);
+            return(true);
+        }
+        catch(err) {
+            await this.sendMessage("Adding reaction role failed. Unexpected error while reacting to message.", message.channel, bot);
+            await this.logger.warning(`Error reacting to message ${reactionMessage.id} in channel ${reactionMessage.channel.id} in guild ${reactionMessage.guild.id}`, err);
+            await bot.configs.reactionRoleConfig.removeReactionRole(reactionRole.guildID, reactionRole.name);
+            return(false);
+        }
+    }
 }
 
 class RemoveReactionRole extends Command {
@@ -140,6 +189,7 @@ class RemoveReactionRole extends Command {
     public async run(bot: PantherBot, message: Message, args: string[]): Promise<CommandResult> {
         if(args.length < 1) {
             return {sendHelp: true, command: this, message: message};
+
         }
 
         let name: string = args[0];
@@ -149,7 +199,7 @@ class RemoveReactionRole extends Command {
             return {sendHelp: false, command: this, message: message};
         }
 
-        let success: boolean = await bot.reactionRoleManager.removeReactionRole(message.guild.id, name, message.client);
+        let success: boolean = await this.removeReactionRole(message.guild.id, name, message, bot);
         if(success) {
             await this.sendMessage(`Reaction role ${name} removed successfully.`, message.channel, bot);
         }
@@ -158,6 +208,58 @@ class RemoveReactionRole extends Command {
         }
 
         return {sendHelp: false, command: this, message: message};
+    }
+
+    private async removeReactionRole(guildId: Snowflake, name: string, message: Message, bot: PantherBot): Promise<boolean> {
+        let removedReactionRole: ReactionRoleObject = await bot.configs.reactionRoleConfig.removeReactionRole(guildId, name);
+
+        if(!removedReactionRole) return(false);
+
+        //Remove our reaction
+        let reaction: MessageReaction = await this.getReaction(removedReactionRole, bot);
+        if(reaction) {
+            try {
+                await reaction.users.remove(message.client.user);
+            }
+            catch(err) {
+                await this.sendMessage("Unexpected error occurred when removing reaction.", message.channel, bot);
+                await this.logger.warning("Error removing reaction from message.", err);
+            }
+        }
+        return(true);
+    }
+
+    private async getReaction(reactionRole: ReactionRoleObject, bot: PantherBot): Promise<MessageReaction> {
+        let channel: TextChannel | NewsChannel;
+        let reactionMessage: Message;
+        let reaction: MessageReaction;
+
+        //Get channel
+        try {
+            channel = <TextChannel | NewsChannel> await bot.client.channels.fetch(reactionRole.channelID);
+        }
+        catch(err) {
+            return(undefined);
+        }
+        //Get message
+        try {
+            reactionMessage = await channel.messages.fetch(reactionRole.messageID);
+        }
+        catch(err) {
+            return(undefined);
+        }
+        //Get reaction
+        try {
+            reaction = reactionMessage.reactions.cache.get(reactionRole.emoteID);
+            if(reaction.partial) {
+                reaction = await reaction.fetch();
+            }
+        }
+        catch(err) {
+            await this.logger.warning(`Error getting reaction ${reactionRole.name} ${reactionRole.emoteID} from message,channel,guild ${reactionRole.messageID}.${reactionRole.channelID},${reactionRole.guildID}`);
+            reaction = undefined;
+        }
+        return(reaction);
     }
 }
 
