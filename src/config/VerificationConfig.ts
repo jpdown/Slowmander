223d7166f225eeb403c0d0b35cfbfab1ccc46aca
Jaden D <24631328+PoisonedPanther@users.noreply.github.com>
2020-08-21 15:26:19 -0500

    add toggle for removing reaction to verification config

--- src/config/VerificationConfig.ts
+++ src/config/VerificationConfig.ts
@@ -2,25 +2,26 @@ import { PantherBot } from "../Bot";
 import { Snowflake } from "discord.js";
 import { DatabaseEntry, DatabaseObject } from "./DatabaseEntry";
 
-export class LockdownConfig extends DatabaseEntry<VerificationConfigObject> {
+export class VerificationConfig extends DatabaseEntry<VerificationConfigObject> {
     private static readonly TABLE: string = "VerificationConfig";
     private static readonly DEFAULT_ENTRY: VerificationConfigObject = undefined;
 
     constructor(bot: PantherBot) {
-        super(LockdownConfig.TABLE, LockdownConfig.DEFAULT_ENTRY, bot);
+        super(VerificationConfig.TABLE, VerificationConfig.DEFAULT_ENTRY, bot);
     }
 
     public async getVerificationConfig(guildID: Snowflake): Promise<VerificationConfigObject> {
         return(await this.getDocument(guildID));
     }
 
-    public async setVerificationConfig(guildID: Snowflake, channelID: Snowflake, messageID: Snowflake, emoteID: Snowflake, roleID: Snowflake): Promise<boolean> {
+    public async setVerificationConfig(guildID: Snowflake, channelID: Snowflake, messageID: Snowflake, emoteID: Snowflake, roleID: Snowflake, removeReaction: boolean): Promise<boolean> {
         let newConfig: VerificationConfigObject = {
             id: guildID,
             channelID: channelID,
             messageID: messageID,
             emoteID: emoteID,
-            roleID: roleID
+            roleID: roleID,
+            removeReaction: removeReaction
         }
         return(await this.updateOrInsertDocument(guildID, newConfig));
     }
@@ -32,5 +33,6 @@ export interface VerificationConfigObject extends DatabaseObject {
     channelID: string,
     messageID: string,
     emoteID: string,
-    roleID: string
+    roleID: string,
+    removeReaction: boolean
 }
