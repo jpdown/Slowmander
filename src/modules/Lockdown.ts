e05cf7d65a95b72ce84c34b897b0f3565028e539
Tyler <kolatra03@gmail.com>
2021-12-30 02:09:57 -0700

    expanded `Lockdown` it doesn't quite work yet as i need to touch up the parsing and figure out discord a bit more, but the skeleton is there. the idea is to be able to provide multiple IDs combined of channels/guilds/categories
    if ur name is merge conflicts don't touch me i hate u

--- src/modules/Lockdown.ts
+++ src/modules/Lockdown.ts
@@ -2,6 +2,8 @@ import type { Bot } from "Bot";
 import type { CommandContext } from "CommandContext";
 import {
     CategoryChannel,
+    Channel,
+    Guild,
     GuildChannel,
     GuildMember,
     MessageEmbed,
@@ -10,10 +12,11 @@ import {
     Role,
     TextBasedChannel,
     TextChannel,
+    VoiceChannel,
 } from "discord.js";
 import { CommandUtils } from "utils/CommandUtils";
 import { Module } from "./Module";
-import { args, command, guild } from "./ModuleDecorators";
+import { args, command, guild, guildOnly } from "./ModuleDecorators";
 
 // TODO handle the command when it's not used as a slash command, and refactor
 export class Lockdown extends Module {
@@ -28,58 +31,133 @@ export class Lockdown extends Module {
     @command("locks a given channel")
     @guild("472222827421106201")
     @args([
-        {
+        /* {
             name: "channel",
             type: "channel",
             description: "the channel to lock",
             optional: true,
+        }, */
+        {
+            name: "channelids",
+            type: "string",
+            description: "list of channel ids to lock",
+            optional: true,
         },
     ])
-    public async lock(c: CommandContext, channel?: GuildChannel) {
-        this.perform(c, true, channel);
+    public async lock(c: CommandContext, m?: string) {
+        // TODO allow giving guild ids as input and figure out how the heck to make these arguments work
+        /*if (channel) {
+            this.perform(c, true, channel.id);
+        } else 
+        if (m) {
+            this.perform(c, true, m);
+        } else {
+            this.perform(c, true, c.channel.id);
+        }*/
+        await c.reply("not working yet");
     }
 
     @command("unlocks a given channel")
     @guild("472222827421106201")
     @args([
-        {
+        /* {
             name: "channel",
             type: "channel",
-            description: "the channel to unlock",
+            description: "the channel to lock",
+            optional: true,
+        }, */
+        {
+            name: "channelids",
+            type: "string",
+            description: "list of channel ids to lock",
             optional: true,
         },
     ])
-    public async unlock(c: CommandContext, channel?: GuildChannel) {
-        this.perform(c, false, channel);
+    public async unlock(c: CommandContext, channel?: GuildChannel, m?: string) {
+        /*if (channel) {
+            this.perform(c, false, channel.id);
+        } else 
+        if (m) {
+            this.perform(c, false, m);
+        } else {
+            this.perform(c, false, c.channel.id);
+        } */
+        await c.reply("not working yet");
     }
 
-    private async perform(
-        c: CommandContext,
-        lock: boolean,
-        channel?: GuildChannel
-    ) {
-        if (!c.guild) return; // TODO temporary check, add a way to make commands guild only
+    private async perform(c: CommandContext, lock: boolean, m: string) {
         await c.defer();
-        if (channel) {
-            await this.updateChannelPerms(
-                channel,
-                [c.guild?.roles.everyone],
-                lock,
-                false,
-                c
-            );
-        } else {
-            await this.updateChannelPerms(
-                c.channel as GuildChannel,
-                [c.guild?.roles.everyone],
-                lock,
-                false,
-                c
-            );
+        let ids = m.split(" ");
+        let channels: GuildChannel[] = [];
+        let failed: string[] = [];
+        let guilds: Guild[] = [];
+        let finalMsg = "";
+        if (ids.length > 0) {
+            // verify we got IDs
+            for (let i = 0; i < ids.length; i++) {
+                let guild = await c.bot.client.guilds.fetch(ids[0]).catch((e) => {
+                    console.log(`MissingAccess on getting guild`, e);
+                });
+                if (guild) {
+                    guilds.push(guild);
+                    continue;
+                }
+                let ch = await c
+                    .guild!.channels.fetch(ids[i])
+                    .catch((e) => console.log(`MissingAccess on getting channel`, e));
+                if (ch instanceof CategoryChannel) {
+                    let catChannels = ch.children;
+                    for (let i = 0; i < catChannels.size; i++) {
+                        let channel = catChannels.at(i);
+                        if (channel instanceof TextChannel || channel instanceof VoiceChannel) {
+                            channels.push(channel);
+                        }
+                    }
+                    continue;
+                }
+                if (ch) {
+                    channels.push(ch);
+                } else {
+                    failed.push(ids[i]);
+                }
+            }
+            if (failed.length > 0) {
+                finalMsg += `\nThe following IDs could not be resolved:\n`;
+                for (let i = 0; i < failed.length; i++) {
+                    finalMsg += `${failed[i]} `;
+                }
+                finalMsg += `\n`;
+            }
         }
-        if (!c.replied) {
-            await c.reply("Channel(s) locked!");
+        if (channels.length > 0) {
+            for (let i = 0; i < channels.length; i++) {
+                let channel = channels[i];
+                if (channel) {
+                    await this.updateChannelPerms(
+                        channel,
+                        [c.guild!.roles.everyone],
+                        lock,
+                        false,
+                        c
+                    );
+                } else {
+                    await this.updateChannelPerms(
+                        c.channel as GuildChannel,
+                        [c.guild!.roles.everyone],
+                        lock,
+                        false,
+                        c
+                    );
+                }
+            }
+            if (!c.replied) {
+                finalMsg += "Channel(s) locked!";
+                await c.reply({ content: finalMsg, ephemeral: true });
+                return;
+            }
+            return;
         }
+        await c.reply({ content: "No channels found from given IDs!", ephemeral: true });
     }
 
     private async updateChannelPerms(
@@ -165,28 +243,20 @@ export class Lockdown extends Module {
         }
         // If category, send in all children recursively
         if (channel.type === "GUILD_CATEGORY") {
-            (channel as CategoryChannel).children.forEach(
-                async (childChannel) => {
-                    if (childChannel.permissionsLocked) {
-                        await this.trySendMessage(childChannel, lock, ctx);
-                    }
+            (channel as CategoryChannel).children.forEach(async (childChannel) => {
+                if (childChannel.permissionsLocked) {
+                    await this.trySendMessage(childChannel, lock, ctx);
                 }
-            );
+            });
             return true;
         }
         // Check perms
-        const member: GuildMember = channel.guild.members.cache.get(
-            bot.client.user!.id
-        )!;
-        if (
-            !channel.permissionsFor(member).has(Permissions.FLAGS.SEND_MESSAGES)
-        ) {
+        const member: GuildMember = channel.guild.members.cache.get(bot.client.user!.id)!;
+        if (!channel.permissionsFor(member).has(Permissions.FLAGS.SEND_MESSAGES)) {
             return false;
         }
         const embed = new MessageEmbed()
-            .setColor(
-                await CommandUtils.getSelfColor(<TextChannel | NewsChannel>channel)
-            )
+            .setColor(await CommandUtils.getSelfColor(<TextChannel | NewsChannel>channel))
             .setDescription(lock ? this.LOCK_MESSAGE : this.UNLOCK_MESSAGE);
 
         if (ctx.channel.id === channel.id) {
