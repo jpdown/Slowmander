aa35cec0a03b889b84155deacf44d5ff5665efe9
Tyler <kolatra03@gmail.com>
2022-01-02 19:02:10 -0700

    small script to create new modules

---
+++ create_file.py
@@ -0,0 +1,17 @@
+import os
+import sys
+
+if len(sys.argv) < 2:
+    print('must provide a name for a file')
+else:
+    fn = sys.argv[1]
+    fp = 'src/modules/{name}.ts'.format(name = fn)
+    if os.path.exists(fp):
+        print('file already exists')
+        sys.exit()
+    else:
+        fn = fn.capitalize()
+        with open(fp, 'w') as nf:
+            nf.write('import { Module } from "./Module";\n\nexport class {0} extends Module {{\n}}'.format(fn))
+        with open('src/modules/index.ts', 'a+') as i:
+            i.write('export {{ {0} }} from "./{0}"\n'.format(fn))
